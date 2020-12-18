823663dcc4790f319cdfe6ea87dc941a5dc320cf
Ricard <ricard.cusso@gmail.com>
2020-12-18 18:01:19 +1000

    Initial commit

---
+++ Stopwatch/Stopwatch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        public DateTime _startTime;
+        public DateTime _stopTime;
+        public bool isRunning = false;
+
+        public void Start()
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+                _startTime = DateTime.Now;
+            }
+        }
+
+        public void Stop()
+        {
+            _stopTime = DateTime.Now;
+            isRunning = false;
+
+        }
+
+        public TimeSpan CalculateTime()
+        {
+            var duration = _stopTime - _startTime;
+            return duration;
+        }
+
+    }
+}
