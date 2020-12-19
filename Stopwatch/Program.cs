0c11fa2b62fbc029c7ecc12fa9e91c3d7a150a03
Daniel Skovli <daniel.skovli@gmail.com>
2020-12-19 22:50:10 +1000

    DS comments for RC

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -8,7 +8,7 @@ namespace Stopwatch
         static void Main(string[] args)
         {
             var stopwatch = new Stopwatch();
-            var loopLimit = 0;
+            var loopLimit = 0; // Unless you plan to use this after your while-loop, just define it when you parse the int
             bool isRunning = true;
 
             while (isRunning)
@@ -17,41 +17,55 @@ namespace Stopwatch
                 Console.WriteLine("How many times would you like to use Stopwatch today?: ");
                 var userInput = Console.ReadLine();
 
+                // This only checks for empty string...
                 if (String.IsNullOrWhiteSpace(userInput))
                 {
                     Console.WriteLine("Must enter a valid number");
-                    continue;
+                    continue; // When this loops back, we get the whole welcome schpiel again
                 }
 
+                // If user input was non-number, this throws a runtime error
                 loopLimit = Convert.ToInt32(userInput);
 
-                    for (var i = 1; i <= loopLimit; i++)
+                for (var i = 1; i <= loopLimit; i++)
+                {
+                    Console.WriteLine("Press Enter to start the Stopwatch:");
+                    Console.ReadLine();
+                    stopwatch.Start();
+
+                    Console.WriteLine("Press Enter to stop the Stopwatch:");
+                    Console.ReadLine();
+
+                    stopwatch.Stop();
+
+                    // Maybe check out string formatting for TimeSpan to see if you can output a nice,
+                    // non-standard representation of how long the stopwatch ran for?
+                    Console.WriteLine($"Stopwatch time is: {stopwatch.CalculateTime()}");
+
+                    if (i != loopLimit)
                     {
-                        Console.WriteLine("Press Enter to start the Stopwatch:");
+                        // I kind of want this <enter> to restart the stopwatch as well, but that's just personal preference
+                        Console.WriteLine("Press Enter to run Stopwatch again:");
                         Console.ReadLine();
-                        stopwatch.Start();
-
-                        Console.WriteLine("Press Enter to stop the Stopwatch:");
-                        Console.ReadLine();
-
-                        stopwatch.Stop();
-                        Console.WriteLine($"Stopwatch time is: {stopwatch.CalculateTime()}");
-
-                        if (i != loopLimit)
-                        {
-                            Console.WriteLine("Press Enter to run Stopwatch again:");
-                            Console.ReadLine();
-                        }
-
-                        else
-                        {
-                            Console.WriteLine("Thank you for using Stopwatch. Press any key to exit: ");
-                            Console.ReadKey();
-                            return;
-                        }
-
                     }
+
+                    else
+                    {
+                        Console.WriteLine("Thank you for using Stopwatch. Press any key to exit: ");
+                        Console.ReadKey();
+
+                        // This makes total sense -- for now.
+                        // But since you've gone through the trouble of binding your loop to a variable,
+                        // it would be smart to utilize this now:
+
+                        // isRunning = false; // This stops the while-loop before next iteration
+                        // break; // This stops the for-loop immediately
+                        return;
+                    }
+                }
             }
+
+            // (future) If you used your `isRunning` to exit the while-loop, you can do cleanup stuff here
         }
     }
 }
--- Stopwatch/Stopwatch.cs
+++ Stopwatch/Stopwatch.cs
@@ -2,20 +2,36 @@
 
 namespace Stopwatch
 {
+
+    // This class is great. Love it! Beers all around :)
+    //
+    // (future) Check out the built-in Stopwatch class for feature ideas:
+    // https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.stopwatch?view=net-5.0
+
     public class Stopwatch
     {
         private DateTime _startTime;
         private DateTime _stopTime;
+
+        // This probably doesn't need to be a `public`, but if you want it to be accessible outside of this
+        // class, I suggest making it a property instead of field. It will make more sense for future you, with
+        // binding stuff to UIs, etc. Promise.
+        // public bool IsRunning { get; set; }
         public bool _isRunning;
 
         public void Start()
         {
+            // I'm a fan of ALWAYS using braces, even for simple stuff:
+            // if (<condition>) {
+            //    <stuff>
+            // }
+            // <clearly not stuff>
+
             if (_isRunning)
                 throw new InvalidOperationException("Stopwatch already running!");
 
             _isRunning = true;
             _startTime = DateTime.Now;
-            
         }
 
         public void Stop()
@@ -31,5 +47,7 @@ namespace Stopwatch
         {
             return _stopTime - _startTime;
         }
+
+        // (future) Look into overriding ToString()
     }
 }
